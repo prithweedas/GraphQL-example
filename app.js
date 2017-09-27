ce0d0a6ea862cf0a8461c6b5aaa1f5537e5f10e9
Prithwee Das <prithweedas123@gmail.com>
2017-09-27 11:57:00 +0530

    added dependencies
    express, graphql & express-graphql

---
+++ app.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { graphqlExpress } from 'apollo-server-express';
+const PORT = 3000;
+var app = express();
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: myGraphQLSchema }));
+app.listen(PORT);
